21c8fcaa2018ff83bbeb2cb8c3ccda80814b76ae
Emmanuel <isiwuemma@gmail.com>
2023-12-17 13:40:11 +0100

    angular icons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapSearch, bootstrapGrid3x3Gap } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgIconComponent],
+  viewProviders: [provideIcons({bootstrapSearch, bootstrapGrid3x3Gap})],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
