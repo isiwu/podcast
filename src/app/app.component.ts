43eab60ed58b97d91fb22b54afb3536fa2b51727
Emmanuel <isiwuemma@gmail.com>
2023-12-19 15:06:07 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { bootstrapSearch, bootstrapGrid3x3Gap } from '@ng-icons/bootstrap-icons';
+import { 
+  bootstrapSearch, 
+  bootstrapGrid3x3Gap, 
+  bootstrapVolumeUpFill, 
+  bootstrapArrowRight } from '@ng-icons/bootstrap-icons';
 import { lucidePodcast } from '@ng-icons/lucide'
 import { ActiveLinkComponent } from './active-link/active-link.component';
 import { simpleSoundcloud } from '@ng-icons/simple-icons';
@@ -11,7 +15,14 @@ import { simpleSoundcloud } from '@ng-icons/simple-icons';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, NgIconComponent, ActiveLinkComponent],
-  viewProviders: [provideIcons({bootstrapSearch, bootstrapGrid3x3Gap, lucidePodcast, simpleSoundcloud})],
+  viewProviders: [provideIcons({
+    bootstrapSearch, 
+    bootstrapGrid3x3Gap, 
+    lucidePodcast, 
+    simpleSoundcloud, 
+    bootstrapVolumeUpFill,
+    bootstrapArrowRight
+  })],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
